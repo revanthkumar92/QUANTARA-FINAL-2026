7cf58285f23e7a0cee1da31c57311fa0e05fa316
Revanth <bandelarevanth013@gmail.com>
2026-02-03 23:33:48 +0530

    Update vite config and add deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/QUANTARA-FINAL-2026/",
   server: {
     host: "::",
     port: 8080,
